7f0fee1e70ea6249e074f05556d0aaf73d9eaf82
Paul Osman <paul@eval.ca>
2015-12-29 11:43:22 -0600

    Add ua-logo alias

--- etc/aliases.zsh
+++ etc/aliases.zsh
@@ -1,5 +1,6 @@
 alias webserver='python -m SimpleHTTPServer'
 alias soundcloud-logo='print -P "%F{172}" && cat ~/.dotfiles/ascii-art/soundcloud && print -P "%f"'
+alias ua-logo='print -P "%F{196}" && cat ~/.dotfiles/ascii-art/ua && print -P "%f"'
 alias ,='ls -alhG'
 alias m=more
 alias c=clear
